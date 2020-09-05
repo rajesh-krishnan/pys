a969452b165f1c1fcbfc8babbab3afacd9098c5e
Rajesh Krishnan <rjsh.krshnn@gmail.com>
2020-09-04 20:47:41 -0400

    nav buttons

--- patanjali.js
+++ patanjali.js
@@ -1,63 +1,289 @@
 /********************************************************************************************************************************/
-var patanjali_data = {
-  1: { "pS": "prathamaH| samAdhipAdaH",   "pE": "Chapter 1: On Concentration",   "cnt": 51 },
-  2: { "pS": "dvitiyaH| sAdhanapAdaH",    "pE": "Chapter 2: On Sadhana",         "cnt": 55 },
-  3: { "pS": "tritiyaH| vibhUtipAdaH",    "pE": "Chapter 3: On Accomplishments", "cnt": 56 },
-  4: { "pS": "chaturthaH| kaivalyapAdaH", "pE": "Chapter 4: On Absoluteness",    "cnt": 34 }
-};
+var patanjali_data = {};
 
-var p = patanjali_data;
-for (j=1; j<=4; j++) { for (i=1; i<=p[j]["cnt"]; i++) p[j][i] = {}; };
+patanjali_data["padas"] = [
+{ "pS": "prathamaH| samAdhipAdaH",   "pE": "Chapter 1: On Concentration",   "cnt": 51 },
+{ "pS": "dvitiyaH| sAdhanapAdaH",    "pE": "Chapter 2: On Sadhana",         "cnt": 55 },
+{ "pS": "tritiyaH| vibhUtipAdaH",    "pE": "Chapter 3: On Accomplishments", "cnt": 56 },
+{ "pS": "chaturthaH| kaivalyapAdaH", "pE": "Chapter 4: On Absoluteness",    "cnt": 34 } 
+];
 
 /********************************************************************************************************************************/
-var x       = "patanjali";
+patanjali_data["patanjali"] = [[
+"atha yogaanushaasanam||1||",
+"yogashchittavRRittinirodhaH||2||",
+"tadaa draShTuH svaruupe'vasthaanam||3||",
+"vRRittisaaruupyamitaratra||4||",
+"vRRittayaH pa~nchatayyaH kliShTaa akliShTaaH||5||",
+"pramaaNaviparyayavikalpanidraasmRRitayaH||6||",
+"pratyakShaanumaanaagamaaH pramaaNaani||7||",
+"viparyayo mithyaaj~naanamatadruupapratiShTham||8||",
+"shabdaj~naanaanupaatii vastushuunyo vikalpaH||9||",
+"abhaavapratyayaalambanaa vRRittirnidraa||10||",
+"anubhuutaviShayaasampramoShaH smRRitiH||11||",
+"abhyaasavairaagyaabhyaaM tannirodhaH||12||",
+"tatra sthitau yatno'bhyaasaH||13||",
+"sa tu diirghakaalanairantaryasatkaaraasevito dRRiDhabhuumiH||14||",
+"dRRiShTaanushravikaviShayavitRRiShNasya vashiikaarasa~nj~naa vairaagyam||15||",
+"tatparaM puruShakhyaaterguNavaitRRiShNyam||16||",
+"vitarkavichaaraanandaasmitaaruupaanugamaatsampraj~naataH ||17||",
+"viraamapratyayaabhyaasapuurvaH saMskaarasheSho'nyaH||18||",
+"bhavapratyayo videhaprakRRitilayaanaam||19||",
+"shraddhaaviiryasmRRitisamaadhipraj~naapuurvaka itareShaam||20||",
+"tiivrasaMvegaanaamaasannaH||21||",
+"mRRidumadhyaadhimaatratvaattato'pi visheShaH||22||",
+"iishvarapraNidhaanaadvaa||23||",
+"kleshakarmavipaakaashayairaparaamRRiShTaH puruShavisheSha iishvaraH||24||",
+"tatra niratishayaM sarvaj~naviijam||25||",
+"puurveShaamapi guruH kaalenaanavachChedaat||26||",
+"tasya vaachakaH praNavaH||27||",
+"tajjapastadarthabhaavanam||28||",
+"tataH pratyakchetanaadhigamo'pyantaraayaabhaavashcha||29||",
+"vyaadhistyaanasaMshayapramaadaalasyaaviratibhraantidarshanaalabdhabhuumikatvaanavasthitatvaani chittavikShepaaste'ntaraayaaH||30||",
+"duHkhadaurmanasyaa~Ngamejayatvashvaasaprashvaasaa vikShepasahabhuvaH||31||",
+"tatpratiShedhaarthamekatattvaabhyaasaH||32||",
+"maitriikaruNaamuditopekShaaNaaM sukhaduHkhapuNyaapuNyaviShayaaNaaM bhaavanaatashchittaprasaadanam||33||",
+"prachChardanavidhaaraNaabhyaaM vaa praaNasya||34||",
+"viShayavatii vaa pravRRittirutpannaa manasaH sthitinibandhinii||35||",
+"vishokaa vaa jyotiShmatii||36||",
+"viitaraagaviShayaM vaa chittam||37||",
+"svapnanidraaj~naanaalambanaM vaa||38||",
+"yathaabhimatadhyaanaadvaa||39||",
+"paramaaNuparamamahattvaanto'sya vashiikaaraH||40||",
+"kShiiNavRRitterabhijaatasyeva maNergrahiitRRigrahaNagraahyeShu tatsthatada~njanataa samaapattiH||41||",
+"shabdaarthaj~naanavikalpaiH sa~NkiirNaa savitarkaa samaapattiH||42||",
+"smRRitiparishuddhau svaruupashuunyevaarthamaatranirbhaasaa nirvitarkaa||43||",
+"etayaiva savichaaraa nirvichaaraa cha suukShmaviShayaa vyaakhyaataa||44||",
+"suukShmaviShayatvaM chaali~Ngaparyavasaanam||45||",
+"taa eva saviijaH samaadhiH||46||",
+"nirvichaaravaishaaradye'dhyaatmaprasaadaH||47||",
+"RRitambharaa tatra praj~naa||48||",
+"shrutaanumaanapraj~naabhyaamanyaviShayaa visheShaarthatvaat||49||",
+"tajjaH saMskaaro'nyasaMskaarapratibandhii||50||",
+"tasyaapi nirodhe sarvanirodhaannirviijaH samaadhiH||51||",
+],[
+"tapaHsvaadhyaayeshvarapraNidhaanaani kriyaayogaH||1||",
+"samaadhibhaavanaarthaH kleshatanuukaraNaarthashcha||2||",
+"avidyaasmitaaraagadveShaabhiniveshaaH pa~ncha kleshaaH||3||",
+"avidyaa kShetramuttareShaaM prasuptatanuvichChinnodaaraaNaam||4||",
+"anityaashuchiduHkhaanaatmasu nityashuchisukhaatmakhyaatiravidyaa||5||",
+"dRRigdarshanashaktyorekaatmatevaasmitaa||6||",
+"sukhaanushayii raagaH||7||",
+"duHkhaanushayii dveShaH||8||",
+"svarasavaahii viduSho'pi tathaaruuDho'bhiniveshaH||9||",
+"te pratiprasavaheyaaH suukShmaaH||10||",
+"dhyaanaheyaastadvRRittayaH||11||",
+"kleshamuulaH karmaashayo dRRiShTaadRRiShTajanmavedaniiyaH||12||",
+"sati muule tadvipaako jaatyaayurbhogaaH||13||",
+"te hlaadaparitaapaphalaaH puNyaapuNyahetutvaat||14||",
+"pariNaamataapasaMskaaraduHkhairguNavRRittivirodhaachcha duHkhameva sarvaM vivekinaH||15||",
+"heyaM duHkhamanaagatam||16||",
+"draShTRRidRRishyayoH saMyogo heyahetuH||17||",
+"prakaashakriyaasthitishiilaM bhuutendriyaatmakaM bhogaapavargaarthaM dRRishyam||18||",
+"visheShaavisheShali~Ngamaatraali~Ngaani guNaparvaaNi||19||",
+"draShTaa dRRishimaatraH shuddho'pi pratyayaanupashyaH||20||",
+"tadartha eva dRRishyasyaatmaa||21||",
+"kRRitaarthaM prati naShTamapyanaShTaM tadanyasaadhaaraNatvaat||22||",
+"svasvaamishaktyoH svaruupopalabdhihetuH saMyogaH||23||",
+"tasya heturavidyaa||24||",
+"tadabhaavaatsaMyogaabhaavo haanaM taddRRisheH kaivalyam||25||",
+"vivekakhyaatiraviplavaa haanopaayaH||26||",
+"tasya saptadhaa praantabhuumiH praj~naa||27||",
+"yogaa~NgaanuShThaanaadashuddhikShaye j~naanadiiptiraavivekakhyaateH||28||",
+"yamaniyamaasanapraaNaayaamapratyaahaaradhaaraNaadhyaanasamaadhayo'ShTaava~Ngaani||29||",
+"ahiMsaasatyaasteyabrahmacharyaaparigrahaa yamaaH||30||",
+"jaatideshakaalasamayaanavachChinnaaH saarvabhaumaa mahaavratam||31||",
+"shauchasantoShatapaHsvaadhyaayeshvarapraNidhaanaani niyamaaH||32||",
+"vitarkabaadhane pratipakShabhaavanam||33||",
+"vitarkaa hiMsaadayaH kRRitakaaritaanumoditaa lobhakrodhamohapuurvakaa mRRidumadhyaadhimaatraa duHkhaaj~naanaanantaphalaa iti pratipakShabhaavanam||34||",
+"ahiMsaapratiShThaayaaM tatsannidhau vairatyaagaH||35||",
+"satyapratiShThaayaaM kriyaaphalaashrayatvam||36||",
+"asteyapratiShThaayaaM sarvaratnopasthaanam||37||",
+"brahmacharyapratiShThaayaaM viiryalaabhaH||38||",
+"aparigrahasthairye janmakathantaasambodhaH||39||",
+"shauchaatsvaa~Ngajugupsaa parairasaMsargaH||40||",
+"sattvashuddhisaumanasyaikaagryendriyajayaatmadarshanayogyatvaani cha||41||",
+"santoShaadanuttamasukhalaabhaH||42||",
+"kaayendriyasiddhirashuddhikShayaattapasaH||43||",
+"svaadhyaayaadiShTadevataasamprayogaH||44||",
+"samaadhisiddhiriishvarapraNidhaanaat||45||",
+"sthirasukhamaasanam||46||",
+"prayatnashaithilyaanantasamaapattibhyaam||47||",
+"tato dvandvaanabhighaataH||48||",
+"tasminsati shvaasaprashvaasayorgativichChedaH praaNaayaamaH||49||",
+"vaahyaabhyantarastambhavRRittiH deshakaalasa~NkhyaabhiH paridRRiShTo diirghasuukShmaH||50||",
+"vaahyaabhyantaraviShayaakShepii chaturthaH||51||",
+"tataH kShiiyate prakaashaavaraNam||52||",
+"dhaaraNaasu cha yogyataa manasaH||53||",
+"svaviShayaasamprayoge chittasya svaruupaanukaara ivendriyaaNaaM pratyaahaaraH||54||",
+"tataH paramaa vashyatendriyaaNaam||55||",
+],[
+"deshabandhashchittasya dhaaraNaa||1||",
+"tatra pratyayaikataanataa dhyaanam||2||",
+"tadevaarthamaatranirbhaasaM svaruupashuunyamiva samaadhiH||3||",
+"trayamekatra saMyamaH||4||",
+"tajjayaatpraj~naalokaH||5||",
+"tasya bhuumiShu viniyogaH||6||",
+"trayamantara~NgaM puurvebhyaH||7||",
+"tadapi vahira~NgaM nirviijasya||8||",
+"vyutthaananirodhasaMskaarayorabhibhavapraadurbhaavau nirodhakShaNachittaanvayo nirodhapariNaamaH||9||",
+"tasya prashaantavaahitaa saMskaaraat||10||",
+"sarvaarthataikaagratayoH kShayodayau chittasya samaadhipariNaamaH||11||",
+"tataH punaH shaantoditau tulyapratyayau chittasyaikaagrataapariNaamaH||12||",
+"etena bhuutendriyeShu dharmalakShaNaavasthaapariNaamaa vyaakhyaataaH||13||",
+"shaantoditaavyapadeshyadharmaanupaatii dharmii||14||",
+"kramaanyatvaM pariNaamaanyatve hetuH||15||",
+"pariNaamatrayasaMyamaadatiitaanaagataj~naanam||16||",
+"shabdaarthapratyayaanaamitaretaraadhyaasaatsa~NkarastatpravibhaagasaMyamaatsarvabhuutarutaj~naanam||17||",
+"saMskaarasaakShaatkaraNaatpuurvajaatij~naanam||18||",
+"pratyayasya parachittaj~naanam||19||",
+"na cha tatsaalambanaM tasyaaviShayiibhuutatvaat||20||",
+"kaayaruupasaMyamaattadgraahyashaktistambhe chakShuHprakaashaasamprayoge'ntardhaanam||21||",
+"sopakramaM nirupakramaM cha karma tatsaMyamaadaparaantaj~naanamariShTebhyo vaa||22||",
+"maitryaadiShu balaani||23||",
+"baleShu hastibalaadiini||24||",
+"pravRRittyaalokanyaasaatsuukShmavyavahitaviprakRRiShTaj~naanam ||25||",
+"bhuvanaj~naanaM suurye saMyamaat||26||",
+"chandre taaraavyuuhaj~naanam||27||",
+"dhruve tadgatij~naanam||28||",
+"naabhichakre kaayavyuuhaj~naanam||29||",
+"kaNThakuupe kShutpipaasaanivRRittiH||30||",
+"kuurmanaaDyaaM sthairyam||31||",
+"muurdhajyotiShi siddhadarshanam||32||",
+"praatibhaadvaa sarvam||33||",
+"hRRidaye chittasaMvit||34||",
+"sattvapuruShayoratyantaasa~NkiirNayoH pratyayaavisheSho bhogaH paraarthatvaatsvaarthasaMyamaatpuruShaj~naanam||35||",
+"tataH praatibhashraavaNavedanaadarshaasvaadavaartaa jaayante||36||",
+"te samaadhaavupasargaa vyutthaane siddhayaH||37||",
+"bandhakaaraNashaithilyaatprachaarasaMvedanaachcha chittasya parashariiraaveshaH||38||",
+"udaanajayaajjalapa~NkakaNTakaadiShvasa~Nga utkraantishcha||39||",
+"samaanajayaajjvalanam||40||",
+"shrotraakaashayoH sambandhasaMyamaaddivyaM shrotram||41||",
+"kaayaakaashayoH sambandhasaMyamaallaghutuulasamaapatteshchaakaashagamanam||42||",
+"vahirakalpitaa vRRittirmahaavidehaa tataH prakaashaavaraNakShayaH||43||",
+"sthuulasvaruupasuukShmaanvayaarthavattvasaMyamaadbhuutajayaH||44||",
+"tato'NimaadipraadurbhaavaH kaayasampattaddharmaanabhighaatashcha||45||",
+"ruupalaavaNyabalavajrasaMhananatvaani kaayasampat||46||",
+"grahaNasvaruupaasmitaanvayaarthavattvasaMyamaadindriyajayaH||47||",
+"tato manojavitvaM vikaraNabhaavaH pradhaanajayashcha||48||",
+"sattvapuruShaanyataakhyaatimaatrasya sarvabhaavaadhiShThaatRRitvaM sarvaj~naatRRitvaM cha||49||",
+"tadvairaagyaadapi doShaviijakShaye kaivalyam||50||",
+"sthaanyupanimantraNe sa~NgasmayaakaraNaM punaraniShTaprasa~Ngaat||51||",
+"kShaNatatkramayoH saMyamaadvivekajaM j~naanam||52||",
+"jaatilakShaNadeshairanyataanavachChedaattulyayostataH pratipattiH||53||",
+"taarakaM sarvaviShayaM sarvathaaviShayamakramaM cheti vivekajaM j~naanam||54||",
+"sattvapuruShayoH shuddhisaamye kaivalyamiti||55||",
+],[
+"janmauShadhimantratapaHsamaadhijaaH siddhayaH||1||",
+"jaatyantarapariNaamaH prakRRityaapuuraat||2||",
+"nimittamaprayojakaM prakRRitiinaaM varaNabhedastu tataH kShetrikavat||3||",
+"nirmaaNachittaanyasmitaamaatraat||4||",
+"pravRRittibhede prayojakaM chittamekamanekeShaam||5||",
+"tatra dhyaanajamanaashayam||6||",
+"karmaashuklaakRRiShNaM yoginastrividhamitareShaam||7||",
+"tatastadvipaakaanuguNaanaamavaabhivyaktirvaasanaanaam||8||",
+"jaatideshakaalavyavahitaanaamapyaanantaryaM smRRitisaMskaarayorekaruupatvaat||9||",
+"taasaamanaaditvaM chaashiSho nityatvaat||10||",
+"hetuphalaashrayaalambanaiH sa~NgRRihiitatvaadeShaamabhaave tadabhaavaH||11||",
+"atiitaanaagataM svaruupato'styadhvabhedaaddharmaaNaam||12||",
+"te vyaktasuukShmaa guNaatmaanaH||13||",
+"pariNaamaikatvaadvastutattvam||14||",
+"vastusaamye chittabhedaattayorvibhaktaH panthaaH||15||",
+"na chaikachittatantraM vastu tadapramaaNakaM tadaa kiM syaat||16||",
+"taduparaagaapekShitvaachchittasya vastu j~naataaj~naatam||17||",
+"sadaa j~naataashchittavRRittayastatprabhoH puruShasyaapariNaamitvaat||18||",
+"na tatsvaabhaasaM dRRishyatvaat||19||",
+"ekasamaye chobhayaanavadhaaraNam||20||",
+"chittaantaradRRishye buddhibuddheratiprasa~NgaH smRRitisa~Nkarashcha||21||",
+"chiterapratisa~Nkramaayaastadaakaaraapattau svabuddhisaMvedanam||22||",
+"draShTRRidRRishyoparaktaM chittaM sarvaartham||23||",
+"tadasa~Nkhyeyavaasanaabhishchitramapi paraarthaM saMhatyakaaritvaat||24||",
+"visheShadarshina aatmabhaavabhaavanaavinivRRittiH||25||",
+"tadaa vivekanimna~Nkaivalyapraagbhaara~nchittam||26||",
+"tachChidreShu pratyayaantaraaNi saMskaarebhyaH||27||",
+"haanameShaaM kleshavaduktam||28||",
+"prasa~Nkhyaane'pyakusiidasya sarvathaa vivekakhyaaterdharmameghaH samaadhiH||29||",
+"tataH kleshakarmanivRRittiH||30||",
+"tadaa sarvaavaraNamalaapetasya j~naanasyaanantyaajj~neyamalpam||31||",
+"tataH kRRitaarthaanaaM pariNaamakramasamaaptirguNaanaam||32||",
+"kShaNapratiyogii pariNaamaaparaantanirgraahyaH kramaH||33||",
+"puruShaarthashuunyaanaaM guNaanaaM pratiprasavaH kaivalyaM svaruupapratiShThaa vaa chitishaktiriti||34||",
+]];
+
+/*
+for (j=1; j<=4; j++) { for (i=1; i<=p[j]["cnt"]; i++) p ["patanjali"] = [j][i]
+*/
+
+/*
 p[1][1][x]  = "atha yogAnushAsanam ||1.1||";
 p[1][2][x]  = "yogashchittavRRittinirodhaH ||1.2||";
 p[1][3][x]  = "tadA drashTuH svarUpe'vasthAnam ||1.3||";
 p[1][4][x]  = "vRRittisArUpyamitaratra ||1.4||";
 p[1][5][x]  = "vRRittayaH pa~nchatayyaH kliShTA aklishTAH ||1.5||";
+p[1][6][x]  = "pramANaviparyayavikalpanidrAsmRRitayaH ||1.6||";
+*/
 
 /********************************************************************************************************************************/
+/*
 var x       = "satchidananda";
 p[1][1][x]  = "Now the exposition of Yoga is being made.";
 p[1][2][x]  = "The restraint of the modifications of the mind-stuff is Yoga.";
+*/
 
 /********************************************************************************************************************************/
+/*
 var x       = "vivekananda";
 p[1][1][x]  = "Now concentration is explained.";
 p[1][2][x]  = "Yoga is restraining the mind - stuff (Chitta) from taking various forms (Vrittis).";
+*/
 
 /********************************************************************************************************************************/
+/*
 var x       = "vyasa";
 p[1][1][x]  = "athetyayamadhikArArthaH | yogAnushAsanaM shAstramadhikRRitaM veditavyam ||";
+*/
 
 /********************************************************************************************************************************/
 var x       = "ganganathP";
+/*
 p[1][1][x]  = "Now (begins) the exposition of Yoga.";
+*/
 
 /********************************************************************************************************************************/
 var x       = "ganganathV";
+/*
 p[1][1][x]  = "Atha denotes Adhikara, i.e., the commencencement of a topic. (The meaning of the Sutra being:) The Science of the instruction of Yoga is to be understood to have begun (here).";
+*/
 
 /********************************************************************************************************************************/
 var x       = "bksiyengar";
+/*
 p[1][1][x]  = "With prayers for divine blessings, now begins an exposition of the sacred art of yoga.";
+*/
 
 /********************************************************************************************************************************/
 var x       = "cisherwood";
+/*
 p[1][1][x]  = "This is the beginning of instruction in yoga.";
+*/
 
 /********************************************************************************************************************************/
 var x       = "tkvdesikachar";
+/*
 p[1][1][x]  = "Here begins the authoritative instruction on Yoga.";
+*/
 
 /********************************************************************************************************************************/
 var x       = "jnanesvara";
+/*
 p[1][1][x]  = "Now, after having done prior preparation through life and other practices, the study and practice of Yoga begins.";
+*/
 
 /********************************************************************************************************************************/
 var x       = "pradeepaka";
+/*
 p[1][1][x]  = "And now begins (atha) the instruction (anusasanam) regarding Yoga (yoga)";
+*/
 
 /********************************************************************************************************************************/
 var x       = "meanings";
